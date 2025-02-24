dbd13212d5cbe3c8b3639f47cb44deb78fafb201
MuhammadWildanf <wildanf.daftar@gmail.com>
2025-02-25 02:01:30 +0700

    update port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ import fs from "fs";
 dotenv.config();
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 const URL = process.env.URL;
 
 const io = new Server(server, {
