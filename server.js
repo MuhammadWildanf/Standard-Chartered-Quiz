5e4a613aeb5b26495e1cd10c7883c34cb40d1fa7
MuhammadWildanf <wildanf.daftar@gmail.com>
2025-02-26 13:50:51 +0700

    fix url

--- server.js
+++ server.js
@@ -322,5 +322,5 @@ const updateData = (data) => {
 
 // Jalankan server
 server.listen(PORT, () => {
-  console.log(`Server berjalan di http://localhost:${PORT}`);
+  console.log(`Server berjalan di https://localhost:${PORT}`);
 });
