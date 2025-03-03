137df581659e7ca32093c7c04b6fec17fbc5b94c
MuhammadWildanf <wildanf.daftar@gmail.com>
2025-03-03 16:29:55 +0700

    update leaderboard

--- server.js
+++ server.js
@@ -414,4 +414,5 @@ const updateData = (data) => {
 // Jalankan server
 server.listen(PORT, () => {
   console.log(`Server berjalan di https://localhost:${PORT}`);
+  // console.log(`Server berjalan di http://192.168.88.116:${PORT}`);
 });
